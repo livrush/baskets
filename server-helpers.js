3c3104c4204b463327c2ea6269a683548046d090
Liv Sabo-Rush <yaninaorellana@Yaninas-MacBook-Pro.local>
2017-12-03 13:51:18 -0800

    move request functions to server-helpers.js

---
+++ server-helpers.js
@@ -0,0 +1,130 @@
+const helpers = {
+  basket: {},
+  ceramic: {},
+  accessory: {},
+}
+
+// !!! //////////////////////////////////////////////////////////////
+// !!! BASKETS //////////////////////////////////////////////////////
+// !!! //////////////////////////////////////////////////////////////
+
+helpers.basket.get = (req, res) => {
+  DB.collection('baskets').find().toArray(function(err, results) {
+    results.sort(function(a, b) {
+      return a.year - b.year;
+    });
+    res.send(results);
+  });
+};
+
+helpers.basket.post = (req, res) => {
+  const basket = prepareBasketInformation(req.body);
+  DB.collection('baskets').save(basket, (err, result) => {
+    if (err) return console.error(err);
+    console.log('Basket saved to database!');
+    res.redirect('/');
+  })
+};
+
+helpers.basket.put = (req, res) => {
+  const basket = prepareBasketInformation(req.body.data);
+  if (req.body.password === process.env.SECRET) {
+    DB.collection('baskets').replaceOne({ _id: ObjectId(req.body.id) }, basket, (err, result) => {
+      if (err) return console.error(err);
+      console.log(`Basket ${req.body.id} update in database!`);
+      res.sendStatus(200);
+    });
+  } else {
+    res.sendStatus(401);
+  }
+};
+
+helpers.basket.delete = (req, res) => {
+  if (req.body.password === process.env.SECRET) {
+    DB.collection('baskets').deleteOne({ _id: ObjectId(req.body.id) }, (err, result) => {
+      if (err) return console.error(err);
+      console.log(`Basket ${req.body.id} deleted from database!`);
+      res.sendStatus(200);
+    });
+  } else {
+    res.sendStatus(401);
+  }
+};
+
+// !!! //////////////////////////////////////////////////////////////
+// !!! CERAMICS /////////////////////////////////////////////////////
+// !!! //////////////////////////////////////////////////////////////
+
+helpers.ceramic.get = (req, res) => {
+  DB.collection('ceramics').find().toArray(function(err, results) {
+    results.sort(function(a, b) {
+      return a.name.localeCompare(b.name);
+    });
+    res.send(results);
+  });
+};
+
+helpers.ceramic.post = (req, res) => {
+  const ceramic = prepareCeramicInformation(req.body);
+  DB.collection('ceramics').save(ceramic, (err, result) => {
+    if (err) return console.error(err);
+    console.log('Ceramic saved to database!');
+    res.redirect('/');
+  })
+};
+
+helpers.ceramic.put = (req, res) => {
+  const ceramic = prepareCeramicInformation(req.body.data);
+  if (req.body.password === process.env.SECRET) {
+    DB.collection('ceramics').replaceOne({ _id: ObjectId(req.body.id) }, ceramic, (err, result) => {
+      if (err) return console.error(err);
+      console.log(`Ceramic ${req.body.id} update in database!`);
+      res.sendStatus(200);
+    });
+  } else {
+    res.sendStatus(401);
+  }
+};
+
+// !!! //////////////////////////////////////////////////////////////
+// !!! CERAMICS /////////////////////////////////////////////////////
+// !!! //////////////////////////////////////////////////////////////
+
+
+// !!! //////////////////////////////////////////////////////////////
+// !!! EXPORTS //////////////////////////////////////////////////////
+// !!! //////////////////////////////////////////////////////////////
+
+module.exports = helpers;
+
+// !!! //////////////////////////////////////////////////////////////
+// !!! HELPER FUNCTIONS /////////////////////////////////////////////
+// !!! //////////////////////////////////////////////////////////////
+
+function makeBoolean (value) {
+  return value === 'true' ? true : false;
+}
+
+function prepareBasketInformation (product) {
+  const updatedProduct = product;
+  updatedProduct.name = product.name.replace(/\b\w/g, l => l.toUpperCase());
+  updatedProduct.type = product.type.replace(/\b\w/g, l => l.toUpperCase());
+  updatedProduct.lid = makeBoolean(product.lid);
+  updatedProduct.liner = makeBoolean(product.liner);
+  updatedProduct.protector = makeBoolean(product.protector);
+  updatedProduct.accessories = makeBoolean(product.accessories);
+  if (updatedProduct['accessories-list']) {
+    updatedProduct['accessories-list'] = product['accessories-list'].split(',');
+  }
+  return updatedProduct;
+}
+
+function prepareCeramicInformation (product) {
+  const updatedProduct = product;
+  updatedProduct.name = product.name.replace(/\b\w/g, l => l.toUpperCase());
+  updatedProduct.type = product.type.replace(/\b\w/g, l => l.toUpperCase());
+  if (updatedProduct['accessories-list']) {
+    updatedProduct['accessories-list'] = product['accessories-list'].split(',');
+  }
+  return updatedProduct;
+}
--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const ObjectId = require('mongodb').ObjectID;
 const MongoClient = require('mongodb').MongoClient;
 const PORT = process.env.PORT;
 const MONGO = process.env.MONGO;
+const helpers = require('/server-helpers');
 let DB = null;
 
 app.use(bodyParser.urlencoded({extended: true}));
@@ -16,83 +17,25 @@ app.use('/', express.static(path.join(__dirname, '/')));
 // !!! BASKETS //////////////////////////////////////////////////////
 // !!! //////////////////////////////////////////////////////////////
 
-app.get('/basket', (req, res) => {
-  DB.collection('baskets').find().toArray(function(err, results) {
-    results.sort(function(a, b) {
-      return a.year - b.year;
-    });
-    res.send(results);
-  });
-});
+app.get('/basket', );
 
-app.post('/basket', (req, res) => {
-  const basket = prepareBasketInformation(req.body);
-  DB.collection('baskets').save(basket, (err, result) => {
-    if (err) return console.error(err);
-    console.log('Basket saved to database!');
-    res.redirect('/');
-  })
-});
+app.post('/basket', );
 
-app.put('/basket', (req, res) => {
-  const basket = prepareBasketInformation(req.body.data);
-  if (req.body.password === process.env.SECRET) {
-    DB.collection('baskets').replaceOne({ _id: ObjectId(req.body.id) }, basket, (err, result) => {
-      if (err) return console.error(err);
-      console.log(`Basket ${req.body.id} update in database!`);
-      res.sendStatus(200);
-    });
-  } else {
-    res.sendStatus(401);
-  }
-});
+app.put('/basket', );
 
-app.delete('/basket', (req, res) => {
-  if (req.body.password === process.env.SECRET) {
-    DB.collection('baskets').deleteOne({ _id: ObjectId(req.body.id) }, (err, result) => {
-      if (err) return console.error(err);
-      console.log(`Basket ${req.body.id} deleted from database!`);
-      res.sendStatus(200);
-    });
-  } else {
-    res.sendStatus(401);
-  }
-});
+app.delete('/basket', );
 
 // !!! //////////////////////////////////////////////////////////////
 // !!! CERAMICS /////////////////////////////////////////////////////
 // !!! //////////////////////////////////////////////////////////////
 
-app.get('/ceramic', (req, res) => {
-  DB.collection('ceramics').find().toArray(function(err, results) {
-    results.sort(function(a, b) {
-      return a.name.localeCompare(b.name);
-    });
-    res.send(results);
-  });
-});
+app.get('/basket', );
 
-app.post('/ceramic', (req, res) => {
-  const ceramic = prepareCeramicInformation(req.body);
-  DB.collection('ceramics').save(ceramic, (err, result) => {
-    if (err) return console.error(err);
-    console.log('Ceramic saved to database!');
-    res.redirect('/');
-  })
-});
+app.post('/basket', );
 
-app.put('/ceramic', (req, res) => {
-  const ceramic = prepareCeramicInformation(req.body.data);
-  if (req.body.password === process.env.SECRET) {
-    DB.collection('ceramics').replaceOne({ _id: ObjectId(req.body.id) }, ceramic, (err, result) => {
-      if (err) return console.error(err);
-      console.log(`Ceramic ${req.body.id} update in database!`);
-      res.sendStatus(200);
-    });
-  } else {
-    res.sendStatus(401);
-  }
-});
+app.put('/basket', );
+
+app.delete('/basket', );
 
 // !!! //////////////////////////////////////////////////////////////
 // !!! DATABASE CONNECTION //////////////////////////////////////////
@@ -102,35 +45,3 @@ MongoClient.connect(MONGO, (err, database) => {
   DB = database;
   app.listen(PORT, function() { console.log(`localhost:${PORT}`) });
 });
-
-// !!! //////////////////////////////////////////////////////////////
-// !!! HELPER FUNCTIONS /////////////////////////////////////////////
-// !!! //////////////////////////////////////////////////////////////
-
-function makeBoolean (value) {
-  return value === 'true' ? true : false;
-}
-
-function prepareBasketInformation (product) {
-  const updatedProduct = product;
-  updatedProduct.name = product.name.replace(/\b\w/g, l => l.toUpperCase());
-  updatedProduct.type = product.type.replace(/\b\w/g, l => l.toUpperCase());
-  updatedProduct.lid = makeBoolean(product.lid);
-  updatedProduct.liner = makeBoolean(product.liner);
-  updatedProduct.protector = makeBoolean(product.protector);
-  updatedProduct.accessories = makeBoolean(product.accessories);
-  if (updatedProduct['accessories-list']) {
-    updatedProduct['accessories-list'] = product['accessories-list'].split(',');
-  }
-  return updatedProduct;
-}
-
-function prepareCeramicInformation (product) {
-  const updatedProduct = product;
-  updatedProduct.name = product.name.replace(/\b\w/g, l => l.toUpperCase());
-  updatedProduct.type = product.type.replace(/\b\w/g, l => l.toUpperCase());
-  if (updatedProduct['accessories-list']) {
-    updatedProduct['accessories-list'] = product['accessories-list'].split(',');
-  }
-  return updatedProduct;
-}
